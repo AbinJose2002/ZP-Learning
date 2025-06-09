1ccdf83765c032bd57be1498ddc78f85565e5cd0
abinjos307@gmail.com <abinjos307@gmail.com>
2025-06-09 10:49:14 +0530

    Slight UI change. Complete button changed to checkbox.

--- to-do-ts/src/components/Hello.tsx
+++ to-do-ts/src/components/Hello.tsx
@@ -28,6 +28,16 @@ export default function Hello({}: Props) {
     useEffect(() => {
     //   console.log(toDoList)
     }, [toDoList])
+
+    const handleUndoComplete = (index:number) => {
+      if(confirm("Are you sure to unmark Complete?")){
+            const updatedTasks = toDoList.map((task, i) =>
+                i === index ? { ...task, complete: false } : task
+        );
+        setToDoList(updatedTasks);
+        localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+        }
+    }
     
     const handleDelete = (indexToRemove:number) => {
         if(confirm("Are you sure to delete?")){
@@ -35,6 +45,7 @@ export default function Hello({}: Props) {
             // console.log(updatedTasks)
             setToDoList(updatedTasks)
             localStorage.setItem('tasks', JSON.stringify(updatedTasks))
+            setTask('')
         }
     }
 
@@ -48,8 +59,8 @@ export default function Hello({}: Props) {
         }
     }
 
-    const handleEdit = (index: number, task: string) => {
-    const editValue = prompt("Enter your new task");
+    const handleEdit = (index: number, task: string | undefined) => {
+    const editValue = prompt("Enter your new task", task);
     if (editValue !== null && editValue.trim() !== "") {
         const updatedTasks = toDoList.map((item, editIndex) =>
             editIndex === index ? { ...item, item: editValue } : item
@@ -63,7 +74,7 @@ export default function Hello({}: Props) {
     <div className='col-8 mx-auto'>
         <div><h1>✏️ To Do List</h1></div><br />
         <div className='d-flex flex-column  col-10 mx-auto justify-content-between'>
-            <input type="text" className='form-control' placeholder='Enter your task' required onChange={(e)=>setTask(e.target.value)}/>
+            <input type="text" className='form-control' placeholder='Enter your task' value={task} required onChange={(e)=>setTask(e.target.value)}/>
             <button className="btn btn-success my-2" onClick={()=>{
                 const tasks:task1 = {
                     item: task,
@@ -72,6 +83,7 @@ export default function Hello({}: Props) {
                 const updatedList = [...toDoList, tasks]
                 setToDoList(updatedList)
                 localStorage.setItem('tasks', JSON.stringify(updatedList))
+                setTask('')
             }}>Add Task</button>
         </div><br />
         <div className="nav-tabs d-flex justify-content-around col-11 mx-auto" style={baseStyle}>
@@ -86,31 +98,38 @@ export default function Hello({}: Props) {
           <div
             key={index}
             className="col-10 mx-auto d-flex justify-content-between align-items-center"
-            style={{ backgroundColor: "#fbebda", borderRadius: "20px", padding: "10px", marginBottom: "10px" }}
+            style={{ backgroundColor: "#59c5fe", borderBottom: "2px solid black", borderRadius: "20px", padding: "10px", marginBottom: "10px" }}
           >
-            <p>{task.item}</p>
+            <div className='d-flex'><input type="checkbox" className='mx-3' onClick={() => handleComplete(index)}/>
+            <p className='mb-0'>{task.item}</p></div>
             <div>
-              <button style={{ border: "none" }} onClick={() => handleEdit(index, task.item)}>🗐</button>
-              <button style={{ border: "none" }} onClick={() => handleDelete(index)}>🗑️</button>
-              <button style={{ border: "none" }} onClick={() => handleComplete(index)}>✔︎</button>
+              <button style={{ border: "none", background: "transparent" }} onClick={() => handleEdit(index, task.item)}>✏️</button>
+              <button style={{ border: "none", background: "transparent" }} onClick={() => handleDelete(index)}>🗑️</button>
+              {/* <button style={{ border: "none", background: "transparent" }} onClick={() => handleComplete(index)}>✔︎</button> */}
+              
             </div>
           </div>
         )
       : task.complete && (
-          <div
-            key={index}
-            className="col-10 mx-auto d-flex justify-content-between align-items-center"
-            style={{ backgroundColor: "#d4edda", borderRadius: "20px", padding: "10px", marginBottom: "10px" }}
-          >
-            <p>{task.item}</p>
-            <div>
-              <button style={{ border: "none" }} onClick={() => handleDelete(index)}>🗑️</button>
-            </div>
-          </div>
+         <div
+  key={index}
+  className="d-flex justify-content-center mb-3"
+>
+  <div
+    className="col-10 d-flex justify-content-between align-items-center" style={{borderBottom: "2px solid black", backgroundColor: "#d4edda", borderRadius: "20px", padding: "10px",}}>
+    <s className="d-flex align-items-center">
+      <div className='d-flex'>
+        <input type="checkbox" className='mx-3' onClick={() => handleUndoComplete(index)} checked={task.complete}/>
+            <p className='mb-0'>{task.item}</p></div>
+    </s>
+    <button style={{ border: "none", background: "transparent" }} onClick={() => handleDelete(index)}>🗑️</button>
+  </div>
+</div>
+
         )
   )}
 </div>
-
+  <p>Where <span style={{color: "green"}}>productivity</span> meets <span style={{color: "blue"}}>clarity</span></p>
     </div>
   )
 }
