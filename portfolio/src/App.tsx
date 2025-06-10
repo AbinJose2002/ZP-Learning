cd681e866a700289d21692f7686332bcd6c4109f
abinjos307@gmail.com <abinjos307@gmail.com>
2025-06-10 14:27:32 +0530

    Personal portfolio project first commit

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './components/Hero'
+import './App.css'
+
+type Props = {}
+
+const App = (props: Props) => {
+  return (
+    <div className='main-body' >
+      <Hero />
+    </div>
+    
+  )
+}
+
+export default App
