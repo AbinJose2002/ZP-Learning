00d2dc29816745d86c9cb6d7dbf1508f76f92d4d
abinjos307@gmail.com <abinjos307@gmail.com>
2025-06-10 10:13:14 +0530

    background image added

--- weather/src/components/Weather.tsx
+++ weather/src/components/Weather.tsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import axios from 'axios'
+import cloudy from '../assets/cloudy.jpg'
+import morning from '../assets/morning.jpg'
+import afternoon from '../assets/afternoon.jpg'
+import evening from '../assets/night.jpg'
 
 type Props = {}
 
@@ -7,7 +11,7 @@ interface locationData {
     name: string;
     region: string;
     country: string;
-    localtime: string | undefined | Date;
+    localtime: string;
 }
 
 interface currentData {
@@ -39,6 +43,7 @@ const Weather = (props: Props) => {
     const [image, setImage] = useState<string>("")
     const [date, setDate] = useState<string>("")
     const [time, setTime] = useState<string>("")
+    const [timeSession, setTimeSession] = useState<string>("Morning")
     const handlePlaceAutoComplete = async () => {
         try {
             const placeFetchRes = await axios.get(`http://api.weatherapi.com/v1/search.json?key=a187b55187bf420391b51741250906&q=${placeName}`)
@@ -53,6 +58,13 @@ const Weather = (props: Props) => {
         }
     }
 
+    const weatherImage : {[key:string]:string} = {
+        cloud : cloudy,
+        Morning: morning,
+        Afternoon: afternoon,
+        Evening: evening
+    }
+
     const searchWeather = async () => {
         try {
             const weatherRes = await axios.get(`http://api.weatherapi.com/v1/current.json?key=a187b55187bf420391b51741250906&q=${placeName}&aqi=no`);
@@ -61,30 +73,45 @@ const Weather = (props: Props) => {
                 setCurrent(weatherRes.data.current)
                 setLocation(weatherRes.data.location)
                 setImage("https:" + weatherRes.data.current.condition.icon)
-                const dateStr: String | Date = location?.localtime; // e.g., "2025-06-09 15:30"
-const dateObj = new Date(dateStr);
+                const dateStr = location?.localtime; // e.g., "2025-06-09 15:30"
+                const dateObj = new Date(dateStr);
 
-// Get just the date
-            const formattedDate = dateObj.toLocaleDateString("en-US", {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-            });
+                let curHr = dateObj.getHours()
 
-            // Get just the time
-            const formattedTime = dateObj.toLocaleTimeString("en-US", {
-            hour: "numeric",
-            minute: "2-digit",
-            hour12: true,
-            });
-            setTime(formattedTime)
-            setDate(formattedDate)
-
-
+                if (curHr < 12) {
+                    setTimeSession("Morning")
+                } else if (curHr < 18) {
+                    setTimeSession("Afternoon")
+                } else {
+                    setTimeSession("Evening")
+                }
+                console.log(time)
+                // Get just the date
+                const formattedDate = dateObj.toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+                });
+                // Get just the time
+                const formattedTime = dateObj.toLocaleTimeString("en-US", {
+                hour: "numeric",
+                minute: "2-digit",
+                hour12: true,
+                });
+                setTime(formattedTime)
+                setDate(formattedDate)
             }
         } catch (error) {
             console.log(error)
         }
+    }
+
+    const cardStyle = { minHeight: "150px",background: "rgba(255, 255, 255, 0.25)",
+        boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
+        backdropFilter: "blur(3.5px)",
+        WebkitBackdropFilter: "blur(3.5px)", // for Safari support
+        borderRadius: "10px",
+        border: "1px solid rgba(255, 255, 255, 0.18)"
     }
 
   return (
@@ -163,22 +190,24 @@ const dateObj = new Date(dateStr);
        <div
   className="col-md-8 col-12 p-4"
   style={{
-    backgroundColor: "#767676",
+    backgroundImage: `url(${weatherImage[timeSession]})`,
+    boxShadow: "var(--box-shadow) inset",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
     borderRadius: "0 20px 20px 0",
     minHeight: "300px",
   }}
 >
   <div className="row text-white">
-    {/* Row 1 */}
     <div className="col-md-4 col-6 mb-3">
-      <div className="p-3 border rounded bg-primary" style={{ minHeight: "150px" }}>
+      <div className="p-3 border rounded" style={cardStyle}>
         <h5>Temperature Feels Like</h5>
         {current?.feelslike_c != null && <p>{current.feelslike_c}<sup> o</sup>C</p>}
       </div>
     </div>
-    
     <div className="col-md-4 col-6 mb-3">
-      <div className="p-3 border rounded bg-primary" style={{ minHeight: "150px" }}>
+      <div className="p-3 border rounded " style={cardStyle}>
         <h5>Wind</h5>
         {current?.wind_kph != null && <p>{current.wind_kph} km/h</p>}
         {current?.wind_dir != null && <p>{current.wind_dir}</p>}
@@ -186,29 +215,28 @@ const dateObj = new Date(dateStr);
     </div>
     
     <div className="col-md-4 col-6 mb-3">
-      <div className="p-3 border rounded bg-primary" style={{ minHeight: "150px" }}>
+      <div className="p-3 border rounded" style={cardStyle}>
         <h5>UV Index</h5>
         {current?.uv != null && <p>{current.uv} </p>}
       </div>
     </div>
 
-    {/* Row 2 */}
     <div className="col-md-4 col-6 mb-3">
-      <div className="p-3 border rounded bg-primary" style={{ minHeight: "150px" }}>
+      <div className="p-3 border rounded" style={cardStyle}>
         <h5>Visibility</h5>
         {current?.vis_km != null && <p>{current.vis_km} km</p>}
       </div>
     </div>
     
     <div className="col-md-4 col-6 mb-3">
-      <div className="p-3 border rounded bg-primary" style={{ minHeight: "150px" }}>
+      <div className="p-3 border rounded" style={cardStyle}>
         <h5>Pressure</h5>
         {current?.pressure_mb != null && <p>{current.pressure_mb} pb</p>}
       </div>
     </div>
     
     <div className="col-md-4 col-6 mb-3">
-      <div className="p-3 border rounded bg-primary" style={{ minHeight: "150px" }}>
+      <div className="p-3 border rounded" style={cardStyle}>
         <h5>SunrCloudise</h5>
         {current?.cloud != null && <p>{current.cloud}</p>}
       </div>
